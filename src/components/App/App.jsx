6e1b57bc0e133bc5e50f075d364729ed31335005
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-05-14 16:13:05 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
